2a45ddb18ad61d0149e0923406976894e7e9ffa2
Felipe <felipefranca824@gmail.com>
2021-04-20 18:26:20 -0300

    Design da tela home finalizado, e criado a appBar da challenge Page

---
+++ lib/challenge/challenge_page.dart
@@ -0,0 +1,22 @@
+import 'package:DevQuiz/challenge/question_indicator/question_indicator_widget.dart';
+import 'package:flutter/material.dart';
+
+class ChallengePage extends StatefulWidget {
+  @override
+  _ChallengePageState createState() => _ChallengePageState();
+}
+
+class _ChallengePageState extends State<ChallengePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(80),
+        child: SafeArea(
+          child: QuestionIndicatorWidget(),
+          top: true,
+        ),
+      ),
+    );
+  }
+}
