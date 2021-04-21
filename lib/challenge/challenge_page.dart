1fdada465058d7730bb639928c5d1f78dfd0845b
Felipe <felipefranca824@gmail.com>
2021-04-20 22:36:11 -0300

    Page de Challenge criada as awnser

--- lib/challenge/challenge_page.dart
+++ lib/challenge/challenge_page.dart
@@ -1,5 +1,7 @@
-import 'package:DevQuiz/challenge/question_indicator/question_indicator_widget.dart';
+import 'package:DevQuiz/challenge/widgets/quiz/quiz_widget.dart';
 import 'package:flutter/material.dart';
+
+import 'widgets/question_indicator/question_indicator_widget.dart';
 
 class ChallengePage extends StatefulWidget {
   @override
@@ -17,6 +19,7 @@ class _ChallengePageState extends State<ChallengePage> {
           top: true,
         ),
       ),
+      body: QuizWidget(title: "O que o Flutter faz em sua totalidade",),
     );
   }
 }
